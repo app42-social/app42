9b82dc959fce08f9279ba9b115176be3690b1063
Julio Faerman <julio@faermanj.com>
2025-09-28 15:54:58 +0000

    wip

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,27 +5,32 @@ set -ex
 apt-get update
 apt-get install -y clang cmake ninja-build pkg-config curl unzip zip xz-utils libglu1-mesa libgtk-3-dev mesa-utils
 
+export $FLUTTER_HOME="/home/vscode/.local/flutter"
+
 # Install Flutter
-if [ ! -d /home/vscode/.local/flutter ]; then
+if [ ! -d $FLUTTER_HOME ]; then
     echo "Installing Flutter..."
     mkdir -p /home/vscode/.local/bin/
-    git clone https://github.com/flutter/flutter.git -b stable /home/vscode/.local/flutter
-    ln -sf /home/vscode/.local/flutter/bin/flutter /home/vscode/.local/bin/
-    ln -sf /home/vscode/.local/flutter/bin/flutter-dev /home/vscode/.local/bin/
-    ln -sf /home/vscode/.local/flutter/bin/dart /home/vscode/.local/bin/   
+    git clone https://github.com/flutter/flutter.git -b stable $FLUTTER_HOME
+    ln -sf $FLUTTER_HOME/bin/flutter /home/vscode/.local/bin/
+    ln -sf $FLUTTER_HOME/bin/flutter-dev /home/vscode/.local/bin/
+    ln -sf $FLUTTER_HOME/bin/dart /home/vscode/.local/bin/
     sudo chown -R vscode:vscode /home/vscode/.local
 fi
 
+export ANDROID_SDK_ROOT="/home/vscode/.local/android-sdk/"
+export ANDROID_TOOLS_URL="https://dl.google.com/android/repository/commandlinetools-linux-13114758_latest.zip"
+
 # Android SDK (command line tools only)
-if [ ! -d /usr/local/android-sdk ]; then
+if [ ! -d $ANDROID_SDK_ROOT ]; then
     echo "Installing Android SDK..."
-    mkdir -p /usr/local/android-sdk
-    cd /usr/local/android-sdk/
-    curl -fLo tools.zip https://dl.google.com/android/repository/commandlinetools-linux-13114758_latest.zip
+    mkdir -p $ANDROID_SDK_ROOT
+    cd $ANDROID_SDK_ROOT
+    curl -fLo tools.zip $ANDROID_TOOLS_URL
     unzip -o tools.zip
-    mv cmdline-tools latest
+    mv cmdline-tools tools
     rm tools.zip
-    /home/vscode/.local/flutter/bin/flutter config --android-sdk /usr/local/android-sdk
+    $FLUTTER_HOME/bin/flutter config --android-sdk $ANDROID_SDK_ROOT
 fi
 
 # if no google-chrome, install it
@@ -39,13 +44,15 @@ if ! command -v google-chrome &> /dev/null; then
     apt-get install -y google-chrome-stable
 fi
 
-export ANDROID_SDK_ROOT="/usr/local/android-sdk/"
 export PATH="$ANDROID_SDK_ROOT/latest/bin":"$PATH"
 export JAVA_HOME="/usr/local/sdkman/candidates/java/current/"
 
 # Install SDK packages
 yes | sdkmanager --licenses --sdk_root=$ANDROID_SDK_ROOT
 sdkmanager --sdk_root=$ANDROID_SDK_ROOT "platform-tools" "platforms;android-34"
+
+# Move ownership to vscode user
+sudo chown -R vscode:vscode /home/vscode/.local
 
 # Run Flutter doctor
 sudo -u vscode /home/vscode/.local/bin/flutter doctor || true
